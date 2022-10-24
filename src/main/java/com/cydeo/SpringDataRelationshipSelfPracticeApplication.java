319a2b98d9ebd53037e510e5c6fa0919caf6aac8
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-25 00:56:49 +0200

    Initial commit

---
+++ src/main/java/com/cydeo/SpringDataRelationshipSelfPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRelationshipSelfPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRelationshipSelfPracticeApplication.class, args);
+    }
+
+}
